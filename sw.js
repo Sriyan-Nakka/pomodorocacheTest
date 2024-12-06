12945eec11e2df4b8d093533c9e31a30eaa8f027
Sriyan-Nakka <sriyannakka@gmail.com>
2024-12-06 21:03:45 +0530

    updated sw again

--- sw.js
+++ sw.js
@@ -1,20 +1,28 @@
 const staticCacheName = "site-static";
 const assets = [
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./Audio/workCycleComplete.wav",
-  "./Audio/breakCycleComplete.mp3",
-  "./Fonts/parkinsans-webfont.woff",
-  "./Fonts/parkinsans-webfont.woff2",
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/Audio/workCycleComplete.wav",
+  "/Audio/breakCycleComplete.mp3",
+  "/Fonts/parkinsans-webfont.woff",
+  "/Fonts/parkinsans-webfont.woff2",
 ];
 
 self.addEventListener("install", (e) => {
-  caches.open(staticCacheName).then((cache) => {
-    cache.addAll(assets);
-  });
+  e.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching...");
+      cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener("fetch", (e) => {
   console.log(`Intercepting fetch request for: ${request.url}`);
+});
+
+self.addEventListener("activate", (e) => {
+  console.log("service worker activated");
 });
