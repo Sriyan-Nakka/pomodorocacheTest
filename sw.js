23c8ee28f8bcaa302f6e16c80a435a9273b572fd
Sriyan-Nakka <sriyannakka@gmail.com>
2024-12-06 20:37:14 +0530

    updated sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
 const staticCacheName = "site-static";
+const assets = [
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./Audio/workCycleComplete.wav",
+  "./Audio/breakCycleComplete.mp3",
+  "./Fonts/parkinsans-webfont.woff",
+  "./Fonts/parkinsans-webfont.woff2",
+];
 
 self.addEventListener("install", (e) => {
-  // e.waitUntil(
-  //   caches.open("static").then((cache) => {
-  //     return cache.addAll([
-  //       "./index.html",
-  //       "./style.css",
-  //       "./script.js",
-  //       "./Audio/workCycleComplete.wav",
-  //       "./Audio/breakCycleComplete.mp3",
-  //       "./Fonts/parkinsans-webfont.woff",
-  //       "./Fonts/parkinsans-webfont.woff2",
-  //     ]);
-  //   })
-  // );
-  caches.open(staticCacheName).then("cache");
+  caches.open(staticCacheName).then((cache) => {
+    cache.addAll(assets);
+  });
 });
 
 self.addEventListener("fetch", (e) => {
