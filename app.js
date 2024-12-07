12c43838f7b592276e50259060fe9fd4aefac028
Sriyan-Nakka <sriyannakka@gmail.com>
2024-12-07 18:00:14 +0530

    removed app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").catch((error) => {
-    console.log(error.message);
-  });
-}
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if("serviceWorker" in navigator){
+  navigator.serviceWorker.register("sw.js").then(registration =>{
+    console.log("SW registered");
+    console.log(registration);
+  }).catch(error => {
+    console.log("SW Registration failed");
+    console.log(error);
+  });
+}
+
 const workCycleCompleteAudio = document.querySelector(
   "#workCycleCompleteAudio"
 );
