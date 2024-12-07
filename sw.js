427b3af13abcc862d998f7ab212d53b59f0bcb20
Sriyan-Nakka <sriyannakka@gmail.com>
2024-12-07 18:12:58 +0530

    changed directory names

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 const staticCacheName = "site-static";
 const assets = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/Audio/workCycleComplete.wav",
-  "/Audio/breakCycleComplete.mp3",
-  "/Fonts/parkinsans-webfont.woff",
-  "/Fonts/parkinsans-webfont.woff2",
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./Audio/workCycleComplete.wav",
+  "./Audio/breakCycleComplete.mp3",
+  "./Fonts/parkinsans-webfont.woff",
+  "./Fonts/parkinsans-webfont.woff2",
 ];
 
 self.addEventListener("install", (e) => {
